beeb566166c972262ea7ea47246fa5fbb6ea3a34
Abusik019 <abusroyale@gmail.com>
2024-04-30 18:33:15 +0300

    fix

--- index.js
+++ index.js
@@ -3,22 +3,64 @@ require('dotenv').config();
 const   mongoose = require('mongoose'),
         chalk = require('chalk'),
         mongoUrl = process.env.MONGO_URL,
-        Worker = require('./models/worker.model');
+        Worker = require('./models/worker.model'),
+        http = require('http'),
+        url_module = require('url');
 
 
-async function main(){
+// Добавление пользователя в бд
+async function main(user){
     await mongoose.connect(mongoUrl)
         .then(() => console.log(chalk.green('Successful connection')));
 
     const worker = new Worker({
-        name: 'Elliot',
-        surname: 'Alderson',
-        age: 25,
-        experience: 7,
-        direction: 'system programmer'
+        name: user.name,
+        surname: user.surname,
+        age: user.age,
+        experience: user.experience,
+        direction: user.direction,
     })
 
     await worker.save();
 }
 
-main().catch(err => console.log(chalk.red(err)));
+
+const { host, port } = process.env
+
+const server = http.createServer((req, res) => {
+    const   { method, url } = req,
+            { pathname, query } = url_module.parse(url, true);
+
+    if(method === "POST" && pathname === "/resume"){
+        let body = [];
+
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        }).on('end', () => {
+            body = Buffer.concat(body).toString();
+
+            try{
+                const { name, surname, age, experience, direction } = JSON.parse(body);
+
+                if (!name || !surname || !age || !experience || !direction) {
+                    res.writeHead(400);
+                    res.end("Invalid data");
+                    return;
+                }
+
+                main({ name, surname, age, experience, direction }).catch(err => console.log(chalk.red(err))); 
+
+                res.writeHead(200)
+                res.end("User added to database")
+            }catch (err) {
+                res.writeHead(400);
+                res.end(err.message)
+            }
+        })
+    }
+})  
+
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
+
